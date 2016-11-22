876c149a4b7d518e6bec33f6dcb0c99d601c467c
boris <boris.karastanev@progress.com>
2016-11-22 17:42:44 +0200

    Implemented a route for creating new Users in the db

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
+// Model name is singular and mongoose searches/creates a plural collection e.g User => users in the mongo cli
 module.exports = mongoose.model('User', new Schema({
     name: String,
     password: String,
--- server.js
+++ server.js
@@ -8,7 +8,8 @@ let mongoose = require('mongoose');
 
 let jwt = require('jsonwebtoken');
 let config = require('./config');
-let User = require('./models/user');
+let UserApi = require('./models/userApi');
+let usrApi = new UserApi();
 
 const PORT = process.env.PORT || config.port;
 
@@ -24,6 +25,25 @@ app.use(morgan('dev'));
 app.get('/', function (req, res) {
     res.send('Hello world');
 });
+
+
+app.get('/addNewUser', function (req, res) {
+    let userData = {
+        name: 'Niki Lauda',
+        password: 'test123',
+        admin: false
+    };
+
+    usrApi.createNewUser(userData, function (err, result) {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(result);
+        }
+    });
+});
+
 
 app.listen(PORT);
 
