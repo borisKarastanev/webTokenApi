029e4695f809187f9fef7f18e50c7e6f5a0c6a90
boris <boris.karastanev@progress.com>
2016-11-22 17:05:13 +0200

    Added basic server implementation.
    The app now have a running server
    
    A port property has been added to the config file
    
    Nodemon has been added to the devDependencies in the package.json

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+    port: 3030,
     secret: 'beemerbenzorbentley',
     database: 'mongodb://localhost/webApi'
 };
--- server.js
+++ server.js
@@ -1,1 +1,30 @@
 'use strict';
+
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
+let mongoose = require('mongoose');
+
+let jwt = require('jsonwebtoken');
+let config = require('./config');
+let User = require('./models/user');
+
+const PORT = process.env.PORT || config.port;
+
+mongoose.connect(config.database);
+
+app.set('authSecret', config.secret);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
+app.get('/', function (req, res) {
+    res.send('Hello world');
+});
+
+app.listen(PORT);
+
+
