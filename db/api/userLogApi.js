905c209877683e084a299c443010cd499388b827
boris <boris.karastanev@progress.com>
2016-11-30 15:19:32 +0200

    Implemented methods for Ip addresses conversion
    _convertIpToInt - coverts ip address to 32bit binary
    
    _covertIpToStr - converts 32 bit binary Ip to dotted version

--- db/api/userLogApi.js
+++ db/api/userLogApi.js
@@ -7,12 +7,29 @@ function UserLogApi() {
 
 // Start Private methods
 
-UserLogApi.prototype._conveertIpToInt = function (ip) {
-    throw new Error('Not implemented yet');
+UserLogApi.prototype._convertIpToInt = function (ip) {
+    if (typeof ip !== 'string' || ip === null) {
+        throw new Error('Ip address must be of type String');
+    }
+
+    ip = ip.split('.');
+
+    return ((((((+ip[0])*256)+(+ip[1]))*256)+(+ip[2]))*256)+(+ip[3]);
 };
 
-UserLogApi.prototype._convertIpToStr = function (ip) {
-    throw new Error('Not implemented yet');
+UserLogApi.prototype._convertIpToStr = function (ipInt) {
+    if (typeof ipInt !== 'number' || ipInt === null) {
+        throw new Error('Ip address must be of type Number');
+    }
+
+    let ipDot = ipInt % 256;
+
+    for (let i = 3; i > 0; i--) {
+        ipInt = Math.floor(ipInt / 256);
+        ipDot = ipInt % 256 + '.' + ipDot;
+    }
+
+    return ipDot;
 };
 
 // End Private methods
